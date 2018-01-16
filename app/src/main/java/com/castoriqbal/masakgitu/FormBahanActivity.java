b5a8104acef667a8de6280fcd3a45eac62e646b2
filiagestalt <silentcantabile@gmail.com>
2018-01-16 22:16:44 +0700

    Revert "AutocompleteTextView Suggestion"
    This reverts commit f8dfb8e6c311dfb9c0ad55d6c5b11a11bdaffd43.

--- app/src/main/java/com/castoriqbal/masakgitu/FormBahanActivity.java
+++ app/src/main/java/com/castoriqbal/masakgitu/FormBahanActivity.java
@@ -3,6 +3,7 @@ package com.castoriqbal.masakgitu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.ActivityUnitTestCase;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
@@ -16,30 +17,22 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.android.volley.Response;
-import com.android.volley.toolbox.StringRequest;
 import com.fujiyuu75.sequent.Animation;
 import com.fujiyuu75.sequent.Sequent;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FormBahanActivity extends AppCompatActivity {
-    private static final String URL_ARRAY_NAMA_BAHAN = "http://masakini.xyz/masakgituapi/namaBahan.php";
     int jumlahBahan;
     LinearLayout linearLayout;
     ArrayAdapter<String> adapter;
     Button button;
     Intent intent;
     ArrayList<String> listBahan = new ArrayList<>();
-    private ArrayList<String> listNamaBahan = new ArrayList<>();
-    Response.Listener<String> listener;
-    StringRequest request;
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain", "Fradulesca", "Beliza"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,36 +43,14 @@ public class FormBahanActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_form_bahan);
 
+
         jumlahBahan = Integer.parseInt(getIntent().getStringExtra("jumlahBahan"));
         linearLayout = findViewById(R.id.linear_layout_form_bahan);
         button = findViewById(R.id.button_search_bahan);
         button.setEnabled(false);
         intent = new Intent(this, SearchActivity.class);
-        adapter = new ArrayAdapter<String>(this, R.layout.simple_dropdown_item, listNamaBahan);
 
-        listener = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                listNamaBahan.clear();
-
-                try {
-                    JSONArray jsonArrayResponse = new JSONArray(response);
-                    for(int i = 0; i < jsonArrayResponse.length(); i++){
-                        listNamaBahan.add(i,jsonArrayResponse.get(i).toString().toString());
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                adapter.clear();
-                adapter.addAll(listNamaBahan);
-                adapter.notifyDataSetChanged();
-            }
-        };
-
-        request = new StringRequest(URL_ARRAY_NAMA_BAHAN, listener, null);
-        VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue().add(request);
-
+        adapter = new ArrayAdapter<String>(this, R.layout.simple_dropdown_item, COUNTRIES);
         for(int i = 0; i < jumlahBahan; i++){
             final AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) getLayoutInflater().inflate(R.layout.auto_complete_text_view,
                     linearLayout, false);
